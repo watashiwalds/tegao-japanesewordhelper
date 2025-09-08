64e1be7d0f55dbbe2524dbf5125ee8d668e7b873
Đức Vinh <vinh221231051@lms.utc.edu.vn>
2025-09-08 18:42:25 +0700

    update MobileNet

--- MobileNet.py
+++ MobileNet.py
@@ -1,116 +1,149 @@
 import os
+import math
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from keras import Model
-from keras.src.applications.mobilenet_v3 import MobileNetV3Small
-from keras.src.layers import GlobalAveragePooling2D, Dropout, Dense
-from sklearn.model_selection import train_test_split
 import cv2
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-from tensorflow.keras.utils import to_categorical
+import keras
+import numpy as np
+from keras.src.applications.mobilenet import MobileNet
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-data_dir = "Out"
+data_dir = "/kaggle/input/image-etl/Out"
 
-# Hàm tiền xử lý ảnh
-def preprocess_image(img_path):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    img_blur = cv2.GaussianBlur(img, (5, 5), 0)
-    _, thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+print("TensorFlow version:", tf.__version__)
+print("GPU Available:", tf.config.list_physical_devices('GPU'))
+
+def process_image(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    crop_margin = 20
+    h, w = img.shape
+    img = img[crop_margin: h - crop_margin, crop_margin: w - crop_margin]
+    kernel = np.ones((5,5), np.uint8)
+    img_erosion = cv2.erode(img, kernel, iterations=0)
+    img_equalized = cv2.equalizeHist(img_erosion)
+    img_gaussian = cv2.GaussianBlur(img_equalized, (5,5), 0)
+
+    _, thresh = cv2.threshold(img_gaussian,0,255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     kernel = np.ones((3,3), np.uint8)
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        valid_contours = [c for c in contours if 200 < cv2.contourArea(c) < 2000]
+        valid_contours = [c for c in contours if 200 < cv2.contourArea(c) < 3000]
         if valid_contours:
             boxes = [cv2.boundingRect(c) for c in valid_contours]
-            x_min = min([x for x, y, w, h in boxes])
-            y_min = min([y for x, y, w, h in boxes])
-            x_max = max([x + w for x, y, w, h in boxes])
-            y_max = max([y + h for x, y, w, h in boxes])
-            img_crop = img[y_min:y_max, x_min:x_max]
+            x_min = min([x for x,y,w,h in boxes])
+            y_min = min([y for x,y,w,h in boxes])
+            x_max = max([x+w for x,y,w,h in boxes])
+            y_max = max([y+h for x,y,w,h in boxes])
+
+            expand = 20
+            x_min = max(0, x_min - expand)
+            y_min = max(0, y_min - expand)
+            x_max = min(w, x_max + expand)
+            y_max = min(h, y_max + expand)
+
+            img_crop = img_gaussian[y_min:y_max, x_min:x_max]
         else:
-            img_crop = img
+            img_crop = img_gaussian
     else:
-        img_crop = img
+        img_crop = img_gaussian
 
-    img_resized = cv2.resize(img_crop, (128, 128))
-    img_norm = img_resized.astype(np.float32) / 255.0
-    return np.expand_dims(img_norm, axis=-1)
+    img_resized = cv2.resize(img_crop, (96, 96))
+    img_norm  = img_resized.astype("float32")/255.0
+    return img_norm
 
+# ----- load data -----
 X = []
 y = []
 
-labels = os.listdir(data_dir)
+labels = sorted(os.listdir(data_dir))
 label_dict = {label: idx for idx, label in enumerate(labels)}
 
 def load_data():
     for label in labels:
-        folder_path = os.path.join(data_dir, label)
-        for file in os.listdir(folder_path):
-            img_path = os.path.join(folder_path, file)
-            try:
-                img = preprocess_image(img_path)
+        if label.startswith("9") or label.startswith("20"):
+            print("Loading:", label)
+            folder_path = os.path.join(data_dir, label)
+            for file in os.listdir(folder_path):
+                image_path = os.path.join(folder_path, file)
+                img = process_image(image_path)
                 X.append(img)
                 y.append(label_dict[label])
-            except Exception as e:
-                print(e)
 
 load_data()
 X = np.array(X)
 y = np.array(y)
 
-print(X.shape, y.shape)
+train_datagen = ImageDataGenerator()
+test_datagen = ImageDataGenerator()
 
+BATCH_SIZE  = 256
+LR = 0.001
+N_EPOCHS = 50
+IMAGE_SIZE = 96
+N_CLASSES = len(labels)
+
+X = np.expand_dims(X, -1)
 X = np.repeat(X, 3, axis=-1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+from keras.utils import to_categorical
 
-y_train = to_categorical(y_train, num_classes=len(labels))
-y_test = to_categorical(y_test, num_classes=len(labels))
+y_train_co = to_categorical(y_train, num_classes=N_CLASSES)
+y_test_co  = to_categorical(y_test, num_classes=N_CLASSES)
 
-base_model = MobileNetV3Small(input_shape=(128, 128, 3), include_top=False, weights='imagenet')
-base_model.trainable = False
+train_gen = train_datagen.flow(X_train, y_train_co, batch_size=BATCH_SIZE, shuffle=True)
+test_gen  = test_datagen.flow(X_test, y_test_co, batch_size=BATCH_SIZE, shuffle=False)
+
+from keras import layers, models
+
+base_model = MobileNet(
+    input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3),
+    include_top=False,
+    weights='imagenet'
+)
 
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dropout(0.3)(x)
-preds = Dense(len(labels), activation='softmax')(x)
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Reshape((1, 1, 1024))(x)
+x = layers.Dropout(0.5)(x)
+x = layers.Conv2D(N_CLASSES, (1, 1), padding='same')(x)
+x = layers.Activation('softmax')(x)
+output = layers.Reshape((N_CLASSES,))(x)
 
-model = Model(inputs=base_model.input, outputs=preds)
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = models.Model(inputs=base_model.input, outputs=output)
 model.summary()
 
-history = model.fit(X_train, y_train, batch_size=32, epochs=10, validation_data=(X_test, y_test))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(learning_rate=LR), metrics=['accuracy'])
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+historys = model.fit(
+    train_gen,
+    epochs=N_EPOCHS,
+    validation_data=test_gen
+)
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+# --- Vẽ training curves ---
+plt.figure(figsize=(12, 5))
 
-y_pred = model.predict(X_test)
-y_pred_classes = np.argmax(y_pred, axis=1)
-y_test_classes = np.argmax(y_test, axis=1)
+plt.subplot(1, 2, 1)
+plt.plot(historys.history['accuracy'])
+plt.plot(historys.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='best')
 
-cm = confusion_matrix(y_test_classes, y_pred_classes)
-
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
-plt.title('Confusion Matrix')
-plt.ylabel('True Label')
-plt.xlabel('Predicted Label')
+plt.subplot(1, 2, 2)
+plt.plot(historys.history['loss'])
+plt.plot(historys.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='best')
 plt.show()

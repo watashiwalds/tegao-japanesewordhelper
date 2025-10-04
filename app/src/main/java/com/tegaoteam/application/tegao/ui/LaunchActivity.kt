d9943197e4f34848a6f2c4dc9ea6ebbb47f48b1c
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-04 09:48:08 +0700

    tryna re-learn how Navigation libs works

--- app/src/main/java/com/tegaoteam/application/tegao/ui/LaunchActivity.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/LaunchActivity.kt
@@ -1,24 +1,17 @@
-package com.tegaoteam.application.tegao.ui.lookup
+package com.tegaoteam.application.tegao.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.tegaoteam.application.tegao.R
-import com.tegaoteam.application.tegao.databinding.ActivityLookupBinding
 
-class LookupActivity : AppCompatActivity() {
-
-    lateinit private var binding: ActivityLookupBinding
-
+class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_lookup)
-
         enableEdgeToEdge()
+        setContentView(R.layout.activity_launch)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/lookup/LookupFragment.kt
@@ -0,0 +1,23 @@
+package com.tegaoteam.application.tegao.ui.lookup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.tegaoteam.application.tegao.R
+import com.tegaoteam.application.tegao.databinding.FragmentLookupBinding
+
+class LookupFragment : Fragment() {
+    lateinit private var binding: FragmentLookupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_lookup, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}

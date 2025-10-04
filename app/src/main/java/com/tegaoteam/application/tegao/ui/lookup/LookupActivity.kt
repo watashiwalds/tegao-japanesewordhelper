25a1838d4045210c0ac1fc5e7928cbbc82e10c83
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-04 09:25:32 +0700

    remember how to use bindingutils

--- app/src/main/java/com/tegaoteam/application/tegao/ui/lookup/LookupActivity.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/lookup/LookupActivity.kt
@@ -1,17 +1,24 @@
-package com.tegaoteam.application.tegao.ui
+package com.tegaoteam.application.tegao.ui.lookup
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.tegaoteam.application.tegao.R
+import com.tegaoteam.application.tegao.databinding.ActivityLookupBinding
 
 class LookupActivity : AppCompatActivity() {
+
+    lateinit private var binding: ActivityLookupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_lookup)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lookup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

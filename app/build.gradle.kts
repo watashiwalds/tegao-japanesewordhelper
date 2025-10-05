1a92ae985a304e542f4958d5560ac54e48d11d74
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-05 10:02:40 +0700

    layout design continue - lookup activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.timber)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

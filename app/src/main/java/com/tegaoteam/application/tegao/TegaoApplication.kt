d7f070f77a93cf3e6c5025e7f0501c7459cee42b
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-08 11:03:09 +0700

    add instance for application context accessing

--- app/src/main/java/com/tegaoteam/application/tegao/TegaoApplication.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/TegaoApplication.kt
@@ -8,6 +8,12 @@ class TegaoApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: Application
+            private set
     }
 
 }

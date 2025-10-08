dd84eef6178095f831d1ff1a135040eb09b5a75a
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-08 08:56:39 +0700

    add id to Dict dataclass

--- app/src/main/java/com/tegaoteam/application/tegao/configs/DictionaryConfig.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/configs/DictionaryConfig.kt
@@ -4,11 +4,8 @@ import org.json.JSONObject
 
 //TODO: Config placeholder, change when doing actual work
 class DictionaryConfig {
-    /*
-
-     */
-
     data class Dict(
+        val id: Int,
         val displayName: String,
         val type: Int,
         val additionalInfo: JSONObject = JSONObject()
@@ -19,8 +16,8 @@ class DictionaryConfig {
         const val DICT_ONLINE = 1
 
         fun getDictionariesList() = listOf(
-            Dict("Jitendex", DICT_LOCAL),
-            Dict("Mazii", DICT_ONLINE)
+            Dict(0, "Jitendex", DICT_LOCAL),
+            Dict(1, "Mazii", DICT_ONLINE)
         )
     }
 }

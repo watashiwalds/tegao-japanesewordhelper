6dd097779213376cf31044f19f2acc644ce1d2a8
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-10 10:07:10 +0700

    wip - todo for dict's retrofit client creation

---
+++ app/src/main/java/com/tegaoteam/application/tegao/data/network/DictionaryRetrofitClient.kt
@@ -0,0 +1,6 @@
+package com.tegaoteam.application.tegao.data.network
+
+class DictionaryRetrofitClient {
+    //TODO: write function to make dictionary's Retrofit client with appropriate searchWord() and searchKanji()
+    //TODO: would need to write a interface in /domain first to uniform /data fetch and /ui display
+}

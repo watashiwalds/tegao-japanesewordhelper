3a4a24201a2872efa9fad42759dae93387b6603e
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-13 22:22:49 +0700

    wip json to domain type Mazii data converter - now as placeholder

--- app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/MaziiJsonConverter.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/MaziiJsonConverter.kt
@@ -1,10 +1,15 @@
 package com.tegaoteam.application.tegao.data.network.converter
 
 import com.google.gson.JsonObject
+import com.tegaoteam.application.tegao.domain.type.Kanji
 import com.tegaoteam.application.tegao.domain.type.Word
 
 object MaziiJsonConverter {
     fun toDomainWordList(json: JsonObject): List<Word> {
+        //TODO
+        return listOf()
+    }
+    fun toDomainKanjiList(json: JsonObject): List<Kanji> {
         //TODO
         return listOf()
     }

8ac16863ffa84d6652bdb013110cb68c03e7e83f
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-13 22:20:48 +0700

    redundant

--- app/src/main/java/com/tegaoteam/application/tegao/data/network/DictionaryRetrofitClient.kt
+++
@@ -1,9 +0,0 @@
-package com.tegaoteam.application.tegao.data.network
-
-import com.tegaoteam.application.tegao.domain.type.Dictionary
-
-class DictionaryRetrofitClient(val dict: Dictionary?) {
-    val retrofit by lazy {
-        if (dict != null) RetrofitMaker.createWithUrl(dict.jsonObject.get(Dictionary.ONL_URL).asString) else null
-    }
-}

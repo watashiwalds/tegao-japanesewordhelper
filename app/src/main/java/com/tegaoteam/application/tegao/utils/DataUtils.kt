0ca71b3f68839b3dfbdc1c5f57c37a1264a28a81
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-15 09:19:15 +0700

    expand JsonObject for toMap conversion

---
+++ app/src/main/java/com/tegaoteam/application/tegao/utils/DataUtils.kt
@@ -0,0 +1,10 @@
+package com.tegaoteam.application.tegao.utils
+
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+
+fun JsonObject.toMap(): Map<String, Any> {
+    val typeToken = object: TypeToken<Map<String, Any>>() {}.type
+    return Gson().fromJson(this, typeToken)
+}

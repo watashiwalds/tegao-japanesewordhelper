0cbea852ed30a53e46cee3eaaf2d58be38bfbbdd
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-15 09:36:24 +0700

    create generic sealed class for repo action's result return

---
+++ app/src/main/java/com/tegaoteam/application/tegao/utils/backend/RepoResult.kt
@@ -0,0 +1,6 @@
+package com.tegaoteam.application.tegao.utils.backend
+
+sealed class RepoResult<out T> {
+    data class Success<T>(val data: T): RepoResult<T>()
+    data class Error<T>(val code: Int? = null, val message: String): RepoResult<T>()
+}

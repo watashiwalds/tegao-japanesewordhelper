8b2f99b2183f6c8bd182380216d2a47fcd621f4d
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-15 15:08:00 +0700

    fill in remained search option function (look a bit repetitive)

--- app/src/main/java/com/tegaoteam/application/tegao/data/hub/DictionaryHub.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/hub/DictionaryHub.kt
@@ -9,14 +9,28 @@ object DictionaryHub {
     private val availApis = DictionaryConfig.getDictionariesApi()
     private fun getDictionaryApiById(dictId: String) = availApis.firstOrNull() { it.dict?.id == dictId }
 
-    suspend fun searchWord(keyword: String): RepoResult<List<Word>> {
-//        TODO("Not yet implemented")
-        return RepoResult.Success(listOf())
+    suspend fun searchWord(keyword: String, dictId: String): RepoResult<List<Word>> {
+        val requestedApi = getDictionaryApiById(dictId)
+        requestedApi?.let {
+            val res = requestedApi.searchWord(keyword)
+            return when (res) {
+                is RepoResult.Error<*> -> res
+                is RepoResult.Success<List<Word>> -> RepoResult.Success(res.data)
+            }
+        }
+        return RepoResult.Error<String>(message = "dictId not found (unsupported or misvalued)")
     }
 
-    suspend fun searchKanji(keyword: String): RepoResult<List<Kanji>> {
-//        TODO("Not yet implemented")
-        return RepoResult.Success(listOf())
+    suspend fun searchKanji(keyword: String, dictId: String): RepoResult<List<Kanji>> {
+        val requestedApi = getDictionaryApiById(dictId)
+        requestedApi?.let {
+            val res = requestedApi.searchKanji(keyword)
+            return when (res) {
+                is RepoResult.Error<*> -> res
+                is RepoResult.Success<List<Kanji>> -> RepoResult.Success(res.data)
+            }
+        }
+        return RepoResult.Error<String>(message = "dictId not found (unsupported or misvalued)")
     }
 
     suspend fun devTest(keyword: String, dictId: String): RepoResult<String> {

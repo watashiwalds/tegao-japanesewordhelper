e325da6fa7b108ed762d9f519de4476718ff4e85
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-15 16:54:03 +0700

    make placeholder data output for converters

--- app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/JishoResponseConverter.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/JishoResponseConverter.kt
@@ -3,14 +3,25 @@ package com.tegaoteam.application.tegao.data.network.converter
 import com.google.gson.JsonObject
 import com.tegaoteam.application.tegao.domain.model.Kanji
 import com.tegaoteam.application.tegao.domain.model.Word
+import okhttp3.ResponseBody
 
 object JishoResponseConverter {
     fun toDomainWordList(json: JsonObject): List<Word> {
         //TODO
-        return listOf()
+        return listOf(Word(
+            id = 0,
+            reading = "Jisho WORD fetching success",
+            furigana = "JSON size: ${json.size()}",
+            definitions = mutableListOf()))
     }
-    fun toDomainKanjiList(json: JsonObject): List<Kanji> {
+    fun toDomainKanjiList(raw: ResponseBody): List<Kanji> {
         //TODO
-        return listOf()
+        return listOf(Kanji(
+            id = 0,
+            character = "Jisho KANJI fetching success",
+            meaning = "RAW size: ${raw.toString().length}",
+            strokeCount = 0,
+            frequency = 0
+        ))
     }
 }
--- app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/MaziiResponseConverter.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/network/converter/MaziiResponseConverter.kt
@@ -7,10 +7,20 @@ import com.tegaoteam.application.tegao.domain.model.Word
 object MaziiResponseConverter {
     fun toDomainWordList(json: JsonObject): List<Word> {
         //TODO
-        return listOf()
+        return listOf(Word(
+            id = 0,
+            reading = "Mazii WORD fetching success",
+            furigana = "JSON size: ${json.size()}",
+            definitions = mutableListOf()))
     }
     fun toDomainKanjiList(json: JsonObject): List<Kanji> {
         //TODO
-        return listOf()
+        return listOf(Kanji(
+            id = 0,
+            character = "Mazii KANJI fetching success",
+            meaning = "JSON size: ${json.size()}",
+            strokeCount = 0,
+            frequency = 0
+        ))
     }
 }

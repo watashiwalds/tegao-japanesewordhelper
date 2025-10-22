c7de65d47fd3916214c6770faef75de752cedd04
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-22 11:28:50 +0700

    code - kanji definition tab rcy basic finished

--- app/src/main/java/com/tegaoteam/application/tegao/ui/component/searchdisplay/KanjiDefinitionTabRecyclerAdapter.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/component/searchdisplay/KanjiDefinitionTabRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package com.tegaoteam.application.tegao.ui.component.searchdisplay
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
@@ -26,19 +25,33 @@ class KanjiDefinitionTabRecyclerAdapter(private val lifecycleOwner: LifecycleOwn
         parent: ViewGroup,
         viewType: Int
     ): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when (viewType) {
+            VIEWTYPE_TABLIST -> TabList.from(lifecycleOwner, parent)
+            else -> CardDisplay.from(lifecycleOwner, parent)
+        }
     }
 
     override fun onBindViewHolder(
         holder: RecyclerView.ViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+        when (holder) {
+            is TabList -> holder.bind(kanjiList)
+            is CardDisplay -> holder.bind(kanjiList.getOrNull(currentCharacterTab))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0) return VIEWTYPE_TABLIST
+        if (position > 0) return VIEWTYPE_CARD
+        return super.getItemViewType(position)
     }
 
     override fun getItemCount(): Int {
         return 2
     }
+
+    private var currentCharacterTab: Int = 0
 
     class TabList private constructor(private val lifecycleOwner: LifecycleOwner, itemView: RecyclerView): RecyclerView.ViewHolder(itemView) {
         fun bind(kanjiList: List<Kanji>) {
@@ -57,47 +70,50 @@ class KanjiDefinitionTabRecyclerAdapter(private val lifecycleOwner: LifecycleOwn
     }
 
     class CardDisplay private constructor(private val lifecycleOwner: LifecycleOwner, private val binding: CardKanjiDefinitionBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(kanji: Kanji) {
-            binding.lifecycleOwner = lifecycleOwner
+        fun bind(kanji: Kanji?) {
+            kanji?.let {
+                binding.lifecycleOwner = lifecycleOwner
 
-            binding.kanjiStroke.text = kanji.character
-            binding.loTagsRcy.adapter = TagGroupListAdapter().apply { submitRawTagList(kanji.tags) }
-            binding.meaning.text = kanji.meaning
+                binding.kanjiStroke.text = kanji.character
+                binding.loTagsRcy.adapter = TagGroupListAdapter().apply { submitRawTagList(kanji.tags) }
+                binding.meaning.text = kanji.meaning
 
-            binding.loKunyomiGrp.toggleVisibility( if (kanji.kunyomi != null) {
-                binding.kunyomi.text = kanji.kunyomi
-                binding.loTagKunyomiIcl.infoTag = TagItem.toTagItem("kunyomi", "Ku")
-                true
-            } else {
-                false
-            })
-            binding.loOnyomiGrp.toggleVisibility( if (kanji.onyomi != null) {
-                binding.onyomi.text = kanji.onyomi
-                binding.loTagOnyomiIcl.infoTag = TagItem.toTagItem("onyomi", "On")
-                true
-            } else {
-                false
-            })
-            binding.loCompositeGrp.toggleVisibility( if (kanji.composites != null) {
-                binding.composite.text = kanji.composites?.joinToString(", ") { "[${it.first}] ${it.second ?: ""}" }
-                //TODO: Globalization by using @string value
-                binding.loTagCompositeIcl.infoTag = TagItem.toTagItem("composite", "Bộ")
-                true
-            } else {
-                false
-            })
+                binding.loKunyomiGrp.toggleVisibility( if (kanji.kunyomi != null) {
+                    binding.kunyomi.text = kanji.kunyomi
+                    binding.loTagKunyomiIcl.infoTag = TagItem.toTagItem("kunyomi", "Ku")
+                    true
+                } else {
+                    false
+                })
+                binding.loOnyomiGrp.toggleVisibility( if (kanji.onyomi != null) {
+                    binding.onyomi.text = kanji.onyomi
+                    binding.loTagOnyomiIcl.infoTag = TagItem.toTagItem("onyomi", "On")
+                    true
+                } else {
+                    false
+                })
+                binding.loCompositeGrp.toggleVisibility( if (kanji.composites != null) {
+                    binding.composite.text = kanji.composites?.joinToString(", ") { "[${it.first}] ${it.second ?: ""}" }
+                    //TODO: Globalization by using @string value
+                    binding.loTagCompositeIcl.infoTag = TagItem.toTagItem("composite", "Bộ")
+                    true
+                } else {
+                    false
+                })
 
-            binding.hasAdditionalInfo = (kanji.additionalInfo != null)
-            val isExpanding = MutableLiveData<Boolean>().apply { value = false }
-            binding.isExpanding = isExpanding
+                val hasAdditionalInfo = (kanji.additionalInfo != null)
+                binding.hasAdditionalInfo = hasAdditionalInfo
+                val isExpanding = MutableLiveData<Boolean>().apply { value = false }
+                binding.isExpanding = isExpanding
 
-            if (binding.hasAdditionalInfo) {
-                val expandFunc = { isExpanding.value = !isExpanding.value!! }
-                listOf(binding.collapseAdditionalInfoImg, binding.loAdditionalInfoAvailableTxt, binding.loExpandClickPaddingImg).forEach { it.setOnClickListener { expandFunc } }
-                binding.loAdditionalInfoRcy.adapter = AdditionalInfoListAdapter().apply { submitList(kanji.additionalInfo) }
+                if (hasAdditionalInfo) {
+                    val expandFunc = { isExpanding.value = !isExpanding.value!! }
+                    listOf(binding.collapseAdditionalInfoImg, binding.loAdditionalInfoAvailableTxt, binding.loExpandClickPaddingImg).forEach { it.setOnClickListener { expandFunc } }
+                    binding.loAdditionalInfoRcy.adapter = AdditionalInfoListAdapter().apply { submitList(kanji.additionalInfo) }
+                }
+
+                binding.executePendingBindings()
             }
-
-            binding.executePendingBindings()
         }
         companion object {
             fun from(lifecycleOwner: LifecycleOwner, parent: ViewGroup): CardDisplay {

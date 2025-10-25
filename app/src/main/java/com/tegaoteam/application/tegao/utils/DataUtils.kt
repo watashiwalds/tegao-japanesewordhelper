9f904b64a10113443af17ae799ee1fa8ee595fd9
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-25 10:38:42 +0700

    code - add getTime function

--- app/src/main/java/com/tegaoteam/application/tegao/utils/DataUtils.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/utils/DataUtils.kt
@@ -3,6 +3,7 @@ package com.tegaoteam.application.tegao.utils
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
+import java.time.Instant
 
 fun JsonObject.toMap(): Map<String, Any> {
     val typeToken = object: TypeToken<Map<String, Any>>() {}.type
@@ -13,3 +14,5 @@ fun String.toSafeQueryString(): String {
     val regex = "[^\\p{L}\\p{Nd}\\s]+"
     return this.replace(Regex(regex), "")
 }
+
+fun getCurrentTimestamp(): Instant? = Instant.now()

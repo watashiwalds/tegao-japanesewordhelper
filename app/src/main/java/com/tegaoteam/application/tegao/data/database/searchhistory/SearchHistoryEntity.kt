47f84c65ef01ced42331b38b09b455b379c132d5
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-26 23:39:44 +0700

    db - use keyword as prim key in search history

--- app/src/main/java/com/tegaoteam/application/tegao/data/database/searchhistory/SearchHistoryEntity.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/database/searchhistory/SearchHistoryEntity.kt
@@ -8,14 +8,13 @@ import com.tegaoteam.application.tegao.domain.model.SearchHistory
 
 @Entity(tableName = Companion.TABLE_NAME)
 data class SearchHistoryEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long,
     @ColumnInfo(name = COL_TYPE) val type: Int,
-    @ColumnInfo(name = COL_KEYWORD) val keyword: String,
+    @PrimaryKey() val keyword: String,
     @ColumnInfo(name = COL_SEARCHDATE) val searchDate: String
 ) {
     fun toDomainSearchHistory() = SearchHistory(type, keyword, searchDate)
     companion object {
-        fun fromDomainSearchHistory(entry: SearchHistory) = SearchHistoryEntity(0, entry.type, entry.keyword, entry.searchDate)
+        fun fromDomainSearchHistory(entry: SearchHistory) = SearchHistoryEntity(entry.type, entry.keyword, entry.searchDate)
 
         const val TABLE_NAME = "search_histories"
         const val COL_TYPE = "type"

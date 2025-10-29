0bfa054b21feee25ce857d72b4322b0d0a89390f
watashiwalds <watashiwalds@users.noreply.github.com>
2025-10-29 17:46:09 +0700

    code - hook optionActivity to option button in mainActivity

--- app/src/main/java/com/tegaoteam/application/tegao/ui/homescreen/MainActivity.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/homescreen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tegaoteam.application.tegao.ui.homescreen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,23 +8,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavOptions
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.tegaoteam.application.tegao.R
 import com.tegaoteam.application.tegao.data.hub.SettingHub
 import com.tegaoteam.application.tegao.databinding.ActivityMainBinding
 import com.tegaoteam.application.tegao.databinding.ItemNavbarIconNLabelChipBinding
 import com.tegaoteam.application.tegao.ui.component.themedchip.ThemedChipListAdapter
+import com.tegaoteam.application.tegao.ui.option.OptionActivity
 import com.tegaoteam.application.tegao.ui.shared.DisplayHelper
 import com.tegaoteam.application.tegao.ui.shared.IdTranslator
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -31,25 +32,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         setupBottomNavbar()
+        bindOptionButton()
     }
 
     private val settingRepo = SettingHub()
     private fun setupBottomNavbar() {
         val navIds = settingRepo.getMainNavbarItemIds()
         val navItems = navIds.mapNotNull { id -> IdTranslator.mainNavbarId(id) }
-        val navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        val navController = (supportFragmentManager.findFragmentById(R.id.mainActFragmentContainerView) as NavHostFragment).navController
         val navOptions = NavOptions.Builder()
             .setLaunchSingleTop(true)
             .setPopUpTo(navController.graph.startDestinationId, false)
             .build()
 
-        binding.navItemsRcy.layoutManager = DisplayHelper.FlexboxLayoutManagerMaker.gridEven(this)
+        _binding.navItemsRcy.layoutManager = DisplayHelper.FlexboxLayoutManagerMaker.gridEven(this)
         val navAdapter = ThemedChipListAdapter(this, ItemNavbarIconNLabelChipBinding::inflate).apply {
             submitListWithClickListener(navItems) { fragmentString ->
                 navController.navigate(fragmentString.toInt(), null, navOptions)
             }
         }
-        binding.navItemsRcy.adapter = navAdapter
-        binding.executePendingBindings()
+        _binding.navItemsRcy.adapter = navAdapter
+        _binding.executePendingBindings()
+    }
+
+    private fun bindOptionButton() {
+        _binding.optionMenuBtn.setOnClickListener {
+            val intent = Intent(this, OptionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

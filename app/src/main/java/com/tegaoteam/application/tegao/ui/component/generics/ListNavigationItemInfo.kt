e60fd64de549486a5a336a4fb6a9d745ac3218cb
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-03 14:55:01 +0700

    xml - design and dataclass for setting item

---
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/component/generics/ListNavigationItemInfo.kt
@@ -0,0 +1,7 @@
+package com.tegaoteam.application.tegao.ui.component.generics
+
+data class ListNavigationItemInfo(
+    val label: String,
+    val detail: String = "",
+    val iconResId: Int = 0
+)

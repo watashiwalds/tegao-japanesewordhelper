12a276a84f4c69834bc334f105a92ab43ea726b2
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-04 22:21:35 +0700

    test - toast when change setting

--- app/src/main/java/com/tegaoteam/application/tegao/data/hub/SettingHub.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/hub/SettingHub.kt
@@ -2,11 +2,15 @@ package com.tegaoteam.application.tegao.data.hub
 
 import com.tegaoteam.application.tegao.data.config.setting.SettingConfig
 import com.tegaoteam.application.tegao.domain.repo.SettingRepo
+import com.tegaoteam.application.tegao.utils.AppToast
 
 class SettingHub: SettingRepo {
     //function settings (mainly boolean or int value)
     override fun isHepburnConverterEnable() = SettingConfig.enableHepburnConverter
-    override suspend fun toggleHepburnConverter() { SettingConfig.toggleHepburnConverter() }
+    override suspend fun toggleHepburnConverter() {
+        SettingConfig.toggleHepburnConverter()
+        AppToast.show("Hepburn toggled", AppToast.LENGTH_SHORT)
+    }
 
     //personalization setting (mainly listOf)
     override fun getMainNavbarItemIds(): List<String> = SettingConfig.mainNavbarItemIds

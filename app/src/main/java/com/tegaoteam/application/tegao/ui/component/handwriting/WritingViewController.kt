dfe6574b8df02b052a9a0a9839390f105544c2b4
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-07 22:35:08 +0700

    code - binding writing full keyboard buttons to edittext (if have) and a placeholder func to ignite when show/hide the board

--- app/src/main/java/com/tegaoteam/application/tegao/ui/component/handwriting/WritingViewController.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/component/handwriting/WritingViewController.kt
@@ -9,8 +9,12 @@ class WritingViewController(
     private val writingView: WritingView,
     onStrokeFinished: ((Bitmap?) -> Unit)? = null,
     private val binding: ViewDataBinding? = null,
-    private val editText: EditText? = null
+    private val editText: EditText? = null,
+    onViewToggled: (() -> Unit)? = null //todo: Hide softKeyboard (if currently show) and do display of the writing view when this invoked
 ) {
+    var nowWriting: Boolean = false
+        private set
+
     init {
         onStrokeFinished?.let { bindingWriteOutputFunction(it) }
         binding?.let { bindingNotNull ->
@@ -38,8 +42,35 @@ class WritingViewController(
     }
 
     private fun bindingEditTextControlFunctions(binding: ViewWritingBoardFullBinding, editText: EditText) {
-        //TODO: Do binding when EditText is presented
-        //include: enterBtn, backspaceBtn, next/prevBtn
+        binding.cursorToLeftBtn.setOnClickListener {
+            editText.setSelection(
+                (editText.selectionStart - 1).let {
+                    if (it < 0) 0 else it
+                }
+            )
+        }
+        binding.cursorToRightBtn.setOnClickListener {
+            editText.setSelection(
+                (editText.selectionEnd + 1).let {
+                    if (it >= editText.text.length-1) editText.text.length else it
+                }
+            )
+        }
+        binding.backspaceCharBtn.setOnClickListener {
+            editText.apply {
+                if (selectionEnd - selectionStart == 0) text.delete(
+                    if (selectionStart == 0) 0 else selectionStart - 1,
+                    selectionStart
+                ) else text.delete(
+                    selectionStart,
+                    selectionEnd
+                )
+            }
+        }
+        binding.enterBtn.setOnClickListener {
+            //TODO: Think about what the enter button should do...
+            editText.clearFocus()
+        }
     }
 
     private fun bindingWriteHelperFunctions(binding: ViewWritingBoardFullBinding) {
@@ -49,6 +80,11 @@ class WritingViewController(
 
     private fun bindingWriteOutputFunction(onStrokeFinished: ((Bitmap?) -> Unit)) {
         writingView.onStrokeFinished = onStrokeFinished
+    }
+
+    fun toggleWritingView() {
+        nowWriting = !nowWriting
+        editText?.apply{ showSoftInputOnFocus = !nowWriting }
     }
 
     companion object {

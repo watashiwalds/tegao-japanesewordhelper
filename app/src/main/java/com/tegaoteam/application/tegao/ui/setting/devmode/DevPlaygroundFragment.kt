305f8be2eb166d388199bc069b17984a78908506
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-07 11:38:30 +0700

    dev - test writingview in bottom gravity (no control on edittext yet)

--- app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
@@ -1,18 +1,34 @@
 package com.tegaoteam.application.tegao.ui.setting.devmode
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.tegaoteam.application.tegao.R
+import com.tegaoteam.application.tegao.databinding.FragmentDevPlaygroundBinding
+import com.tegaoteam.application.tegao.ui.component.handwriting.WritingViewController
+import timber.log.Timber
 
 class DevPlaygroundFragment : Fragment() {
+    private lateinit var _binding: FragmentDevPlaygroundBinding
+    private lateinit var _writingController: WritingViewController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_dev_playground, container, false)
+        _writingController = WritingViewController(
+            writingView = _binding.loWritingViewIcl.writingPadWrv,
+            onStrokeFinished = { bitmap -> Timber.i("Bitmap received with value ${bitmap}") },
+            binding = _binding.loWritingViewIcl,
+            editText = _binding.testEdittextEdt
+        )
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dev_playground, container, false)
+        return _binding.root
     }
 }

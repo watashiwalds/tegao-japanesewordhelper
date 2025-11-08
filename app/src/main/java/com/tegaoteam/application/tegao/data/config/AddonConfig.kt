5f953cf7c3247224d395acad5e843dfda4c4ac7d
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-08 17:30:35 +0700

    dev - temporarily set handwriting config to enable (view testing)

--- app/src/main/java/com/tegaoteam/application/tegao/data/config/AddonConfig.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/config/AddonConfig.kt
@@ -1,5 +1,11 @@
 package com.tegaoteam.application.tegao.data.config
 
 object AddonConfig {
-    val isHandwritingAvailable = false
+    private fun handwritingModuleAvailability(): Boolean {
+        //todo: Research PackageManager to check for com.tegaoteam.tegao.addon.handwritingrecognition
+        //temporal enable for WritingView testing
+        return true
+    }
+
+    val isHandwritingAvailable = handwritingModuleAvailability()
 }

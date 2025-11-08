7811855953432f7e1108e925a3fdfbb7701ef552
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-08 14:29:16 +0700

    dev - test (a lot) with keyboard and edit focusing behaviors

--- app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
@@ -1,14 +1,17 @@
 package com.tegaoteam.application.tegao.ui.setting.devmode
 
-import android.graphics.Bitmap
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import com.tegaoteam.application.tegao.R
 import com.tegaoteam.application.tegao.databinding.FragmentDevPlaygroundBinding
+import com.tegaoteam.application.tegao.ui.component.generics.SwitchButtonInfo
 import com.tegaoteam.application.tegao.ui.component.handwriting.WritingViewController
 import timber.log.Timber
 
@@ -24,9 +27,29 @@ class DevPlaygroundFragment : Fragment() {
         _writingController = WritingViewController(
             writingView = _binding.loWritingViewIcl.writingPadWrv,
             onStrokeFinished = { bitmap -> Timber.i("Bitmap received with value ${bitmap}") },
-            binding = _binding.loWritingViewIcl,
+            writingBoard = _binding.loWritingViewIcl,
             editText = _binding.testEdittextEdt
         )
+
+        _binding.testPushswitch.apply {
+            switchInfo = SwitchButtonInfo(
+                iconResId = R.drawable.ftc_round_tick_128,
+                switchState = MutableLiveData<Boolean>().apply { value = false },
+                onStateChangedListener = { state ->
+                    _writingController.toggleWritingMode(state)
+                    activity?.apply {
+                        if (currentFocus == _binding.testEdittextEdt)
+                            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+                                if (state) hideSoftInputFromWindow(_binding.testEdittextEdt.windowToken, 0)
+                                else showSoftInput(_binding.testEdittextEdt, 0)
+                            }
+                    }
+                    Timber.i("Writing mode toggled with state [$state]")
+                }
+            )
+            lifecycleOwner = viewLifecycleOwner
+            executePendingBindings()
+        }
 
         // Inflate the layout for this fragment
         return _binding.root

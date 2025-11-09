4492312c99b1edefa01455faecd8c2aeeed66a7e
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-09 16:31:06 +0700

    code - include handwritingAddonApi var to addonRepo

--- app/src/main/java/com/tegaoteam/application/tegao/data/hub/AddonHub.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/hub/AddonHub.kt
@@ -1,8 +1,11 @@
 package com.tegaoteam.application.tegao.data.hub
 
+import com.tegaoteam.application.tegao.data.addon.HandwritingAddonApi
 import com.tegaoteam.application.tegao.data.config.AddonConfig
+import com.tegaoteam.application.tegao.domain.interf.AlternativeInputApi
 import com.tegaoteam.application.tegao.domain.repo.AddonRepo
 
-class AddonHub: AddonRepo {
+class AddonHub : AddonRepo {
     override fun isHandwritingAvailable(): Boolean = AddonConfig.isHandwritingAvailable
+    override val handwritingAddonApi = if (isHandwritingAvailable()) HandwritingAddonApi.instance else null
 }
--- app/src/main/java/com/tegaoteam/application/tegao/domain/repo/AddonRepo.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/domain/repo/AddonRepo.kt
@@ -1,5 +1,8 @@
 package com.tegaoteam.application.tegao.domain.repo
 
+import com.tegaoteam.application.tegao.domain.interf.AlternativeInputApi
+
 interface AddonRepo {
     fun isHandwritingAvailable(): Boolean
+    val handwritingAddonApi: AlternativeInputApi?
 }

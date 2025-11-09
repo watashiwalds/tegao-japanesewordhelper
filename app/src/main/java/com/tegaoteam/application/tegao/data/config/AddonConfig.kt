c4a12a7d2ea94bf4cb1d6bcc455a86ae0cea5566
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-09 16:05:09 +0700

    code - add addon package check (specifically handwriting recognition addon)

--- app/src/main/java/com/tegaoteam/application/tegao/data/config/AddonConfig.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/config/AddonConfig.kt
@@ -1,11 +1,21 @@
 package com.tegaoteam.application.tegao.data.config
 
+import android.content.pm.PackageManager
+import com.tegaoteam.application.tegao.TegaoApplication
+
 object AddonConfig {
-    private fun handwritingModuleAvailability(): Boolean {
-        //todo: Research PackageManager to check for com.tegaoteam.tegao.addon.handwritingrecognition
-        //temporal enable for WritingView testing
-        return true
+    private fun checkHandwritingAvailability(): Boolean {
+        val result = try {
+            TegaoApplication.instance.applicationContext.packageManager.getPackageInfo(
+                "com.tegaoteam.addon.tegao.handwritingrecognition",
+                0
+            )
+            true
+        } catch (_: PackageManager.NameNotFoundException) {
+            false
+        }
+        return result
     }
 
-    val isHandwritingAvailable = handwritingModuleAvailability()
+    val isHandwritingAvailable = checkHandwritingAvailability()
 }

dd5e35505418c3702cedf303a1dd4d73410314b2
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-15 11:08:45 +0700

    wip - show section label in addon setting frag

--- app/src/main/java/com/tegaoteam/application/tegao/ui/setting/addon/SettingAddonFragment.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/setting/addon/SettingAddonFragment.kt
@@ -5,18 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.tegaoteam.application.tegao.R
 import com.tegaoteam.application.tegao.data.hub.SettingHub
+import com.tegaoteam.application.tegao.databinding.FragmentSettingAddonBinding
+import com.tegaoteam.application.tegao.domain.repo.SettingRepo
+import com.tegaoteam.application.tegao.ui.setting.SettingEntryListAdapter
 
 class SettingAddonFragment : Fragment() {
+    private lateinit var _binding: FragmentSettingAddonBinding
     private lateinit var _viewModel: SettingAddonViewModel
+    private lateinit var _adapter: SettingEntryListAdapter
+    private lateinit var _settingRepo: SettingRepo
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _viewModel = ViewModelProvider(this, SettingAddonViewModel.Companion.ViewModelFactory(SettingHub()))[SettingAddonViewModel::class.java]
-        return inflater.inflate(R.layout.fragment_setting_addon, container, false)
+        _settingRepo = SettingHub()
+        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_setting_addon, container, false)
+        _viewModel = ViewModelProvider(this, SettingAddonViewModel.Companion.ViewModelFactory(_settingRepo))[SettingAddonViewModel::class.java]
+
+        _adapter = SettingEntryListAdapter(viewLifecycleOwner).apply {
+            submitList(_viewModel.addonSettings)
+        }
+        _binding.loSettingListLst.adapter = _adapter
+        _binding.executePendingBindings()
+        return _binding.root
     }
 }
--- app/src/main/java/com/tegaoteam/application/tegao/ui/setting/addon/SettingAddonViewModel.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/setting/addon/SettingAddonViewModel.kt
@@ -2,10 +2,23 @@ package com.tegaoteam.application.tegao.ui.setting.addon
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.tegaoteam.application.tegao.R
 import com.tegaoteam.application.tegao.domain.repo.SettingRepo
+import com.tegaoteam.application.tegao.ui.setting.model.ConfigEntryItem
 
 class SettingAddonViewModel(private val _settingRepo: SettingRepo) : ViewModel() {
-    // TODO: Implement the ViewModel
+    val addonSettings = listOf(
+        ConfigEntryItem(
+            labelResId = R.string.setting_addon_label_section_toggle,
+            descriptionResId = 0,
+            type = ConfigEntryItem.Companion.Type.NON_CONTROL
+        ),
+        ConfigEntryItem(
+            labelResId = R.string.setting_addon_label_section_status,
+            descriptionResId = 0,
+            type = ConfigEntryItem.Companion.Type.NON_CONTROL
+        )
+    )
 
     companion object {
         class ViewModelFactory(

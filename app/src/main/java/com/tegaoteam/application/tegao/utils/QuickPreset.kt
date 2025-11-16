26db0c64eb9a4b988986178cf4052f1fac14f4aa
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-16 17:58:24 +0700

    fix - wrongfully setTitle for message value

--- app/src/main/java/com/tegaoteam/application/tegao/utils/QuickPreset.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/utils/QuickPreset.kt
@@ -6,21 +6,17 @@ import androidx.appcompat.app.AlertDialog
 import com.tegaoteam.application.tegao.R
 
 object QuickPreset {
-    fun requestConfirmation(context: Context, titleId: Any? = null, messageId: Any? = null, lambdaRun: (() -> Unit)? = null) {
+    fun requestConfirmation(context: Context, title: Any? = null, message: Any? = null, lambdaRun: (() -> Unit)? = null) {
         AlertDialog.Builder(context).apply {
-            titleId?.let {
+            title?.let {
                 if (it is String) setTitle(it)
                 if (it is Int) setTitle(it)
             }
-            messageId?.let {
-                if (it is String) setTitle(it)
-                if (it is Int) setTitle(it)
+            message?.let {
+                if (it is String) setMessage(it)
+                if (it is Int) setMessage(it)
             }
-            setPositiveButton(R.string.phrase_confirm, object: DialogInterface.OnClickListener {
-                override fun onClick(p0: DialogInterface?, p1: Int) {
-                    lambdaRun?.invoke()
-                }
-            })
+            setPositiveButton(R.string.phrase_confirm) { p0, p1 -> lambdaRun?.invoke() }
             setNegativeButton(R.string.phrase_cancel, null)
         }.show()
     }

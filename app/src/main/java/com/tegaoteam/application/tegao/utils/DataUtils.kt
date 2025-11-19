9ac169dcffbbd157e15a23c8e97aabb02f5c7d31
watashiwalds <watashiwalds@users.noreply.github.com>
2025-11-19 09:38:42 +0700

    utils - logical day difference calculate

--- app/src/main/java/com/tegaoteam/application/tegao/utils/DataUtils.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/utils/DataUtils.kt
@@ -7,6 +7,7 @@ import com.google.gson.reflect.TypeToken
 import java.security.MessageDigest
 import java.time.Instant
 import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 fun JsonObject.toMap(): Map<String, Any> {
     val typeToken = object: TypeToken<Map<String, Any>>() {}.type
@@ -19,25 +20,35 @@ fun String.toSafeQueryString(): String {
 }
 
 object Time {
+    private fun transformToInstant(value: Any?): Instant? {
+        return try {
+            when (value) {
+                is String -> Instant.parse(value)
+                is Instant -> value
+                else -> getCurrentTimestamp()
+            }
+        } catch (_: Exception) {
+            getCurrentTimestamp()
+        }
+    }
     fun getCurrentTimestamp(): Instant? = Instant.now()
     fun timeDifferenceBetween(start: Any?, end: Any?, differenceIn: Int): Long {
-        val cStart = when (start) {
-            is String -> Instant.parse(start)
-            is Instant -> start
-            else -> getCurrentTimestamp()
-        }
-        val cEnd = when (end) {
-            is String -> Instant.parse(end)
-            is Instant -> end
-            else -> getCurrentTimestamp()
-        }
+        val cStart = transformToInstant(start)
+        val cEnd = transformToInstant(end)
         val durationDiff = Duration.between(cStart, cEnd)
         return when (differenceIn) {
             DIFF_DAY -> durationDiff.toDays()
-            else -> durationDiff.toMinutes()
+            else -> 0
         }
     }
-
+    fun logicalTimeDifferenceBetween(start: Any?, end: Any?, differenceIn: Int): Long {
+        val cStart = transformToInstant(start)
+        val cEnd = transformToInstant(end)
+        return when (differenceIn) {
+            DIFF_DAY -> ChronoUnit.DAYS.between(cStart, cEnd)
+            else -> 0
+        }
+    }
     const val DIFF_DAY = 0
 }
 

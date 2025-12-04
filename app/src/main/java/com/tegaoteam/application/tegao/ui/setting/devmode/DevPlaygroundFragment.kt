59def9f0244251cb87d9408d100acbf189575c83
watashiwalds <watashiwalds@users.noreply.github.com>
2025-12-04 10:30:02 +0700

    dev - SRSCalculation test

--- app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/setting/devmode/DevPlaygroundFragment.kt
@@ -11,6 +11,7 @@ import com.tegaoteam.application.tegao.R
 import com.tegaoteam.application.tegao.databinding.FragmentDevPlaygroundBinding
 import com.tegaoteam.application.tegao.domain.model.CardEntry
 import com.tegaoteam.application.tegao.domain.model.CardRepeat
+import com.tegaoteam.application.tegao.ui.learning.cardlearn.learnrun.SRSCalculation
 import timber.log.Timber
 
 class DevPlaygroundFragment : Fragment() {
@@ -30,21 +31,57 @@ class DevPlaygroundFragment : Fragment() {
     }
 
     private lateinit var viewModel: DevPlaygroundViewModel
-    private val cardsOfGroups = mutableListOf<CardRepeat>()
+    private lateinit var testRepeat: CardRepeat
     private fun initObservers() {
-        viewModel.evMakePrint.beacon.observe(viewLifecycleOwner) {
-            if (viewModel.evMakePrint.receive()) {
-                _binding.textView.text = cardsOfGroups.joinToString("\n") { "${it.cardId}: \n\t${it.lastRepeat} \n\t${it.nextRepeat} \n\t${it.easeFactor}" }
-            }
+        viewModel.groups.observe(viewLifecycleOwner) {
+            viewModel.queryCardsByGroupId(it.first().groupId)
         }
         viewModel.queriedCardRepeat.observe(viewLifecycleOwner) {
-            cardsOfGroups.addAll(it)
-            Timber.i("1 batch fin, count: ${it.size}")
-            viewModel.evMakePrint.ignite()
+            testRepeat = it.first()
+            initTestRating()
         }
-        viewModel.groups.observe(viewLifecycleOwner) {
-            it.forEach { group -> viewModel.queryCardsByGroupId(group.groupId) }
-            Timber.i("All query request sent")
+    }
+
+    private lateinit var srsCal: SRSCalculation
+    private fun initTestRating() {
+        srsCal = SRSCalculation()
+        srsCal.calculateRepeat(testRepeat)
+
+        _binding.apply {
+            repeatDataTxt.text = "${testRepeat.cardId}\n\t${testRepeat.lastRepeat}\n\t${testRepeat.nextRepeat}\n\t${testRepeat.easeFactor}"
+
+            ratingEasyBtn.apply {
+                srsCal.srsText_easy.observe(viewLifecycleOwner) { text = it }
+                setOnClickListener {
+                    val newRpt = srsCal.makeRepeatOfRating(SRSCalculation.RATING_EASY)
+                    srsCal.calculateRepeat(newRpt)
+                    repeatDataTxt.text = "${newRpt.cardId}\n\t${newRpt.lastRepeat}\n\t${newRpt.nextRepeat}\n\t${newRpt.easeFactor}"
+                }
+            }
+            ratingGoodBtn.apply {
+                srsCal.srsText_good.observe(viewLifecycleOwner) { text = it }
+                setOnClickListener {
+                    val newRpt = srsCal.makeRepeatOfRating(SRSCalculation.RATING_GOOD)
+                    srsCal.calculateRepeat(newRpt)
+                    repeatDataTxt.text = "${newRpt.cardId}\n\t${newRpt.lastRepeat}\n\t${newRpt.nextRepeat}\n\t${newRpt.easeFactor}"
+                }
+            }
+            ratingHardBtn.apply {
+                srsCal.srsText_hard.observe(viewLifecycleOwner) { text = it }
+                setOnClickListener {
+                    val newRpt = srsCal.makeRepeatOfRating(SRSCalculation.RATING_HARD)
+                    srsCal.calculateRepeat(newRpt)
+                    repeatDataTxt.text = "${newRpt.cardId}\n\t${newRpt.lastRepeat}\n\t${newRpt.nextRepeat}\n\t${newRpt.easeFactor}"
+                }
+            }
+            ratingForgetBtn.apply {
+                srsCal.srsText_forget.observe(viewLifecycleOwner) { text = it }
+                setOnClickListener {
+                    val newRpt = srsCal.makeRepeatOfRating(SRSCalculation.RATING_FORGET)
+                    srsCal.calculateRepeat(newRpt)
+                    repeatDataTxt.text = "${newRpt.cardId}\n\t${newRpt.lastRepeat}\n\t${newRpt.nextRepeat}\n\t${newRpt.easeFactor}"
+                }
+            }
         }
     }
 }

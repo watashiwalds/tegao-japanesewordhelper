9b23842cd5f3d81fe15dff3700c9278241be94bc
watashiwalds <watashiwalds@users.noreply.github.com>
2025-12-04 22:55:30 +0700

    wip - xml design for learningMetrics

---
+++ app/src/main/java/com/tegaoteam/application/tegao/ui/learning/cardlearn/model/ReviewHistoryInfo.kt
@@ -0,0 +1,8 @@
+package com.tegaoteam.application.tegao.ui.learning.cardlearn.model
+
+data class ReviewHistoryInfo(
+    val id: Long,
+    val label: String,
+    val quickInfo: String,
+    val dueIn: Int
+)

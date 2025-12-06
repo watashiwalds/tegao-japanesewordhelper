9020359d916dfcc24fc75912ef9d61ece1cea3ec
watashiwalds <watashiwalds@users.noreply.github.com>
2025-12-06 17:34:31 +0700

    rename - DefaultSettingValue to DefaultValue for potential use with others configs

--- app/src/main/java/com/tegaoteam/application/tegao/data/config/datastore/DefaultValues.kt
+++ app/src/main/java/com/tegaoteam/application/tegao/data/config/datastore/DefaultValues.kt
@@ -1,6 +1,6 @@
 package com.tegaoteam.application.tegao.data.config.datastore
 
-object DefaultSettingValue {
+object DefaultValues {
     const val USE_HEPBURN_CONVERTER = true
     const val ENABLE_HANDWRITING_ADDON = true
 }
